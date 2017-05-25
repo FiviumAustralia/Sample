d51880bc5e8984df260a946d242b7267ef921e68
biskyt <toby.bisco@openeyes.org.uk>
2017-05-25 08:52:31 +0100

    added vf demo script

---
+++ sql/demo/visualfieldsimport.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /var/www/openeyes/protected
+
+rm -rf runtime/fields
+
+mkdir runtime/fields
+mkdir runtime/fields/legacy
+mkdir runtime/fields/out
+mkdir runtime/fields/err
+mkdir runtime/fields/dups
+
+cp modules/OphInVisualfields/tests/fields/legacy/* runtime/fields/legacy/
+
+./yiic importlegacyvf import --importDir=runtime/fields/legacy --archiveDir=runtime/fields/out --errorDir=runtime/fields/err --dupDir=runtime/fields/dups
+
+chmod -R a+rw files
