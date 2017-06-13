cba28e47122ca70e10f35d6488f7d5223616fe3a
biskyt <toby.bisco@openeyes.org.uk>
2017-06-13 16:35:55 +0100

    update settings

--- sql/demo/990-DemoSystemSettings.sql
+++ sql/demo/990-DemoSystemSettings.sql
@@ -5,5 +5,8 @@
 ************************************************************************/
 
 /*settings */
+UPDATE `openeyes`.`setting_installation` SET `value`='.please@set.me.' WHERE `id`='1';
+UPDATE `openeyes`.`setting_installation` SET `value`='.please@set.me.' WHERE `id`='2';
 UPDATE `openeyes`.`setting_installation` SET `value`='' WHERE `id`='3';
-UPDATE `openeyes`.`setting_installation` SET `value`='01234 567 890' WHERE `id`='4';
+UPDATE `openeyes`.`setting_installation` SET `value`='<a href="http://www.abehr.com" target="_blank">www.abehr.com</a>' WHERE `id`='4';
+UPDATE `openeyes`.`setting_installation` SET `value`='.please@set.me.' WHERE `id`='6';
