dee032dfbad168834fd45bf0affbfa6238a8e0dc
biskyt <toby.bisco@openeyes.org.uk>
2017-06-13 12:50:38 +0100

    fixed some addresses

--- sql/demo/Demofixes.sql
+++ sql/demo/Demofixes.sql
@@ -11,3 +11,7 @@ INSERT INTO authassignment (itemname, userid) VALUES ('Patient Tickets admin', 6
 -- Add a glaucoma management plan to the glaucoma test patients
 INSERT INTO `et_ophciexamination_overallmanagementplan` (`event_id`, `left_target_iop_id`, `right_target_iop_id`, `gonio_id`, `clinic_interval_id`, `photo_id`, `oct_id`, `hfa_id`, `hrt_id`, `eye_id`) VALUES ('244', '15', '15', '2', '1', '1', '4', '5', '1', '3');
 INSERT INTO `et_ophciexamination_overallmanagementplan` (`event_id`, `left_target_iop_id`, `right_target_iop_id`, `gonio_id`, `clinic_interval_id`, `photo_id`, `oct_id`, `hfa_id`, `hrt_id`, `eye_id`) VALUES ('3373284', '15', '15', '2', '1', '1', '4', '5', '1', '3');
+
+INSERT INTO `address` (`address1`, `city`, `postcode`, `county`, `country_id`, `contact_id`) VALUES ('1 The street', 'Townsville', 'TW1 1NN', 'Essex', '1', '9489');
+INSERT INTO `address` (`address1`, `address2`, `city`, `postcode`, `county`, `country_id`, `contact_id`) VALUES ('River Cottage', '123 The lane', 'Hull', 'HU1 1LL', 'Berkshire', '1', '8031');
+INSERT INTO `address` (`address1`, `city`, `postcode`, `county`, `country_id`, `contact_id`) VALUES ('74 Brommels Road', 'London', 'SW4 1AB', 'London', '1', '8092');
