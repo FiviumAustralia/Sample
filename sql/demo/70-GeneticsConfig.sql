af7a215bf61209bd5ade07e74f2e1a96cca2434d
biskyt <toby.bisco@openeyes.org.uk>
2017-06-21 16:01:48 +0100

    Start setting-up demo users and permissions

--- sql/demo/70-GeneticsConfig.sql
+++ sql/demo/70-GeneticsConfig.sql
@@ -7,7 +7,7 @@ use openeyes;
 INSERT INTO `contact` (`primary_phone`, `title`, `first_name`, `last_name`) VALUES ('01234567890', 'Mr', 'Genetics', 'Clinical');
 INSERT INTO `contact` (`title`, `first_name`, `last_name`) VALUES ('Mr', 'Genetics', 'Administrator');
 INSERT INTO `contact` (`title`, `first_name`, `last_name`) VALUES ('Mr', 'Genetics', 'Lab Tech');
-INSERT INTO `contact` (`title`, `first_name`, `last_name`) VALUES ('Mrs', 'CVI', 'Clerical');
+INSERT INTO `contact` (`title`, `first_name`, `last_name`) VALUES ('Mr', 'Genetics', 'User');
 
 -- Create Users
 INSERT INTO `user` (`username`, `first_name`, `last_name`, `active`, `global_firm_rights`, `role`, `password`, `salt`, `is_clinical`, `contact_id`)
@@ -20,4 +20,27 @@ INSERT INTO `user` (`username`, `first_name`, `last_name`, `active`, `global_fir
 SELECT 'geneticstech', 'Genetics', 'Lab Tech', '1', '1', '', 'b0c6e884403a5c05bd634943df1b9a08', '7jMG6UQ2dY', '1', (SELECT id from contact c WHERE c.first_name = 'Genetics' AND c.last_name = 'Lab Tech');
 
 INSERT INTO `user` (`username`, `first_name`, `last_name`, `active`, `global_firm_rights`, `role`, `password`, `salt`, `is_clinical`, `contact_id`)
-SELECT 'cviclerical', 'CVI', 'Clerical', '1', '1', '', 'b0c6e884403a5c05bd634943df1b9a08', '7jMG6UQ2dY', '1', (SELECT id from contact c WHERE c.first_name = 'CVI' AND c.last_name = 'Clerical');
+SELECT 'geneticsuser', 'Genetics', 'User', '1', '1', '', 'b0c6e884403a5c05bd634943df1b9a08', '7jMG6UQ2dY', '1', (SELECT id from contact c WHERE c.first_name = 'Genetics' AND c.last_name = 'User');
+
+-- Set permissions
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Genetics Clinical', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Clinical');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'User', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Clinical');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'View clinical', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Clinical');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Edit', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Clinical');
+
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Genetics Admin', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Administrator');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'User', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Administrator');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'View clinical', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Administrator');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Edit', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Administrator');
+
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Genetics Laboratory Technician', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Lab Tech');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'User', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Lab Tech');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'View clinical', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Lab Tech');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Edit', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='Lab Tech');
+
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Genetics User', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='User');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'User', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='User');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'View clinical', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='User');
+INSERT INTO `openeyes`.`authassignment` (`itemname`, `userid`) SELECT'Edit', (SELECT id FROM user WHERE first_name = 'Genetics' AND last_name='User');
+
+UPDATE user set last_firm_id = 5;
--- sql/demo/Demofixes.sql
+++ sql/demo/Demofixes.sql
@@ -4,10 +4,6 @@
 * and can be removed from this file
 *******/
 
-/* add missing patient ticketing right for demo user */
-INSERT INTO authassignment (itemname, userid) VALUES ('Patient Tickets', 6006);
-INSERT INTO authassignment (itemname, userid) VALUES ('Patient Tickets admin', 6006);
-
 -- Add a glaucoma management plan to the glaucoma test patients
 INSERT INTO `et_ophciexamination_overallmanagementplan` (`event_id`, `left_target_iop_id`, `right_target_iop_id`, `gonio_id`, `clinic_interval_id`, `photo_id`, `oct_id`, `hfa_id`, `hrt_id`, `eye_id`) VALUES ('244', '15', '15', '2', '1', '1', '4', '5', '1', '3');
 INSERT INTO `et_ophciexamination_overallmanagementplan` (`event_id`, `left_target_iop_id`, `right_target_iop_id`, `gonio_id`, `clinic_interval_id`, `photo_id`, `oct_id`, `hfa_id`, `hrt_id`, `eye_id`) VALUES ('3373284', '15', '15', '2', '1', '1', '4', '5', '1', '3');
@@ -18,6 +14,3 @@ INSERT INTO `address` (`address1`, `city`, `postcode`, `county`, `country_id`, `
 INSERT INTO `address` (`address1`, `city`, `postcode`, `county`, `country_id`, `contact_id`) VALUES ('24 Peticoat Lane', 'London', 'EC2 1QQ', 'London', '1', '9498');
 INSERT INTO `address` (`address1`, `city`, `postcode`, `county`, `country_id`, `contact_id`) VALUES ('1 The street', 'Townsville', 'TW1 1NN', 'Essex', '1', '9499');
 INSERT INTO `address` (`address1`, `city`, `country_id`, `contact_id`) VALUES ('The North Pole', 'Lapland', '76', '8095');
-
--- Add all user roles to admin user
-INSERT INTO authassignment (itemname, userid) SELECT name, 1 FROM openeyes.authitem where type=2 AND name not in (SELECT itemname from authassignment where userid=1) AND name != 'API access';
