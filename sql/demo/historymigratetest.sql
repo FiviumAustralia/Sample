5b187f5bf793af749c6d189f2d9514d8758b81de
biskyt <toby.bisco@openeyes.org.uk>
2017-06-30 12:22:12 +0100

    full history for 1 patient

--- sql/demo/historymigratetest.sql
+++ sql/demo/historymigratetest.sql
@@ -9,3 +9,33 @@ INSERT INTO `patient_allergy_assignment` (`patient_id`, `allergy_id`, `last_modi
 -- Test history of edits
 DELETE FROM `patient_allergy_assignment_version` WHERE id=1448;
 INSERT INTO `patient_allergy_assignment_version` (`id`,`patient_id`,`allergy_id`,`last_modified_user_id`,`last_modified_date`,`created_user_id`,`created_date`,`version_date`,`version_id`,`other`,`comments`) VALUES (1448,1990875,4,1,'2016-04-04 04:04:04',1,'2016-04-04 04:04:04','2017-06-29 10:56:25',13,NULL,'Test Migration 4');
+
+-- Past Ophtalmic Surgery
+INSERT INTO `previous_operation` (`patient_id`, `side_id`, `operation`, `date`) VALUES ('1990875', '1', 'Refractive Surgery', '2011-11-12');
+INSERT INTO `previous_operation` (`patient_id`, `side_id`, `operation`, `date`) VALUES ('1990875', '2', 'Extraction of foreign body from cornea', '2015-02-04');
+INSERT INTO `previous_operation` (`patient_id`, `side_id`, `operation`, `date`) VALUES ('1990875', '3', 'Exfoliation of lashes', '2016-10-03');
+
+
+-- Systemic diagnoses
+INSERT INTO `secondary_diagnosis` (`disorder_id`, `eye_id`, `patient_id`, `date`) VALUES ('253005', '1', '1990875', '2017-01-01');
+INSERT INTO `secondary_diagnosis` (`disorder_id`, `patient_id`, `date`) VALUES ('486006', '1990875', '2017-02-02');
+INSERT INTO `secondary_diagnosis` (`disorder_id`, `eye_id`, `patient_id`, `date`) VALUES ('3044008', '3', '1990875', '2017-03-03');
+INSERT INTO `secondary_diagnosis` (`disorder_id`, `patient_id`, `date`) VALUES ('284223003', '1990875', '2017-04-05');
+
+
+-- Risks
+INSERT INTO `patient_risk_assignment` (`patient_id`, `risk_id`, `comments`) VALUES ('1990875', '1', 'Test 1');
+INSERT INTO `patient_risk_assignment` (`patient_id`, `risk_id`, `comments`) VALUES ('1990875', '2', 'Test 2');
+INSERT INTO `patient_risk_assignment` (`patient_id`, `risk_id`, `comments`) VALUES ('1990875', '3', 'Test 3');
+INSERT INTO `patient_risk_assignment` (`patient_id`, `risk_id`, `comments`, `other`) VALUES ('1990875', '5', 'Test 4', 'Undergoing Clinical Trials');
+
+-- Family History
+INSERT INTO `family_history` (`patient_id`, `relative_id`, `side_id`, `condition_id`, `comments`) VALUES ('1990875', '1', '1', '1', 'Test Comment');
+INSERT INTO `family_history` (`patient_id`, `relative_id`, `side_id`, `condition_id`) VALUES ('1990875', '2', '2', '2');
+INSERT INTO `family_history` (`patient_id`, `relative_id`, `side_id`, `condition_id`, `comments`, `other_relative`, `other_condition`) VALUES ('1990875', '10', '4', '7', 'Test custom relative and condition', 'Cousin twice removed', 'Stroke');
+
+--
+INSERT INTO `socialhistory` (`patient_id`, `occupation_id`, `smoking_status_id`, `accommodation_id`, `comments`, `carer_id`, `alcohol_intake`, `substance_misuse_id`)
+VALUES ('1990875', '1', '1', '1', 'Sheltered accommodation - Carer visits twice daily', '1', '31', '1');
+INSERT INTO `socialhistory` (`patient_id`, `smoking_status_id`, `accommodation_id`, `comments`, `carer_id`)
+VALUES ('1935214', '1', '1', 'Sheltered accommodation - Carer visits twice daily', '1' );
