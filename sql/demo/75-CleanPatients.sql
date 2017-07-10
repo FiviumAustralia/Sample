a3c423c68e3982832b95f8fa671ce729e5cf512a
biskyt <toby.bisco@openeyes.org.uk>
2017-07-10 11:15:22 +0100

    Set patient names 100900 - 1009099

--- sql/demo/75-CleanPatients.sql
+++ sql/demo/75-CleanPatients.sql
@@ -13,15 +13,15 @@ UPDATE `contact` SET `last_name`='Windsor', `maiden_name`='Mountbatten' WHERE `i
 UPDATE `contact` SET `first_name`='Karl', `last_name`='Marx' WHERE `id`='9691';
 UPDATE `contact` SET `title`='Mr', `first_name`='Martin', `last_name`='Luther King' WHERE `id`='104763';
 UPDATE `contact` SET `title`='Mr', `first_name`='Cristopher', `last_name`='Columbus' WHERE `id`='111963';
-UPDATE `contact` SET `first_name`='Isaac', `last_name`='Newton' WHERE `id`='118685';
+UPDATE `contact` SET `title`='Sir', `first_name`='Isaac', `last_name`='Newton' WHERE `id`='118685';
 UPDATE `contact` SET `first_name`='Theodore', `last_name`='Roosevelt' WHERE `id`='121690';
 UPDATE `contact` SET `first_name`='Ludvig', `last_name`='Beethoven' WHERE `id`='133080';
-UPDATE `contact` SET `title`='Mr', `first_name`='Winston', `last_name`='Churchill' WHERE `id`='164912';
+UPDATE `contact` SET `title`='Sir', `first_name`='Winston', `last_name`='Churchill' WHERE `id`='164912';
 UPDATE `contact` SET `title`='Mrs', `first_name`='Mary', `last_name`='Magdalene' WHERE `id`='173982';
-UPDATE `contact` SET `title`='Mrs', `first_name`='Jacquline', `last_name`='Kennedy', `maiden_name`='Onassis' WHERE `id`='244422';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Jacquline', `last_name`='Kennedy Onassis', `maiden_name`='Onassis' WHERE `id`='244422';
 UPDATE `contact` SET `title`='Miss', `first_name`='Marilyn', `last_name`='Monroe' WHERE `id`='277931';
 UPDATE `contact` SET `title`='Miss', `first_name`='Anne', `last_name`='Frank' WHERE `id`='378007';
-UPDATE `contact` SET `first_name`='Catherine', `last_name`='Middleton' WHERE `id`='8093';
+UPDATE `contact` SET `title` = 'HRH', `first_name`='Catherine', `last_name`='Duchess of Cambridge', `maiden_name` = 'Middleton' WHERE `id`='8093';
 
 UPDATE `contact` SET `first_name`='Wolfgang', `last_name`='Mozart' WHERE `id`='9087';
 UPDATE `contact` SET `first_name`='Ulysses', `last_name`='Grant' WHERE `id`='9088';
@@ -50,7 +50,7 @@ UPDATE `contact` SET `first_name`='Elvis', `last_name`='Presley' WHERE `id`='911
 UPDATE `contact` SET `first_name`='Vincent', `last_name`='Van Gogh' WHERE `id`='9111';
 UPDATE `contact` SET `last_name`='Dickinson' WHERE `id`='9112';
 UPDATE `contact` SET `first_name`='Nicolaus', `last_name`='Copernicus' WHERE `id`='9113';
-UPDATE `contact` SET `title`='Mr', `first_name`='John', `last_name`='Lenin' WHERE `id`='9114';
+UPDATE `contact` SET `title`='Mr', `first_name`='John', `last_name`='Lennon' WHERE `id`='9114';
 UPDATE `contact` SET `title`='Mr', `first_name`='Oscar', `last_name`='Wilde' WHERE `id`='9115';
 UPDATE `contact` SET `first_name`='Richard', `last_name`='Nixon' WHERE `id`='9116';
 UPDATE `contact` SET `title`='Mr', `first_name`='Nikola', `last_name`='Tesla' WHERE `id`='9117';
@@ -58,13 +58,76 @@ UPDATE `contact` SET `first_name`='Harry', `last_name`='Truman' WHERE `id`='9118
 UPDATE `contact` SET `title`='Miss', `first_name`='Joan', `last_name`='Of Arc' WHERE `id`='9119';
 UPDATE `contact` SET `first_name`='Otto', `last_name`='Von Bismarck' WHERE `id`='9120';
 UPDATE `contact` SET `first_name`='John', `last_name`='Locke' WHERE `id`='9121';
+UPDATE `contact` SET `first_name`='Isambard Kingdom', `last_name`='Brunel' WHERE `id`='9122';
+UPDATE `contact` SET `title`='Lady', `first_name`='Diana', `last_name`='Spencer', `maiden_name`='Frances' WHERE `id`='9123';
+UPDATE `contact` SET `title`='Lord', `first_name`='Horatio', `last_name`='Nelson' WHERE `id`='9124';
+UPDATE `contact` SET `title`='Captain', `first_name`='James', `last_name`='Cook' WHERE `id`='9125';
+UPDATE `contact` SET `title`='Mr', `first_name`='Robert', `last_name`='Baden-Powell' WHERE `id`='9126';
+UPDATE `contact` SET `title`='Lady', `first_name`='Margaret', `last_name`='Thatcher' WHERE `id`='9127';
+UPDATE `contact` SET `title`='Sir', `first_name`='Paul', `last_name`='McCartney' WHERE `id`='9128';
+UPDATE `contact` SET `title`='Sir', `first_name`='Alexander', `last_name`='Flemming' WHERE `id`='9129';
+UPDATE `contact` SET `first_name`='Alan', `last_name`='Turing' WHERE `id`='9130';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Emmeline', `last_name`='Pankhurst' WHERE `id`='9131';
+UPDATE `contact` SET `title`='Mr', `first_name`='William', `last_name`='Wilberforce' WHERE `id`='9132';
+UPDATE `contact` SET `first_name`='David', `last_name`='Bowie' WHERE `id`='9133';
+UPDATE `contact` SET `first_name`='Guy', `last_name`='Fawkes' WHERE `id`='9134';
+UPDATE `contact` SET `first_name`='Eric', `last_name`='Morecambe' WHERE `id`='9135';
+UPDATE `contact` SET `title`='Mr', `first_name`='David', `last_name`='Beckham' WHERE `id`='9136';
+UPDATE `contact` SET `first_name`='Victoria', `last_name`='Beckham' WHERE `id`='9137';
+UPDATE `contact` SET `title`='Mr', `first_name`='Thomas', `last_name`='Paine' WHERE `id`='9138';
+UPDATE `contact` SET `title`='Sir', `first_name`='Steve', `last_name`='Redgrave' WHERE `id`='9139';
+UPDATE `contact` SET `title`='Sir', `first_name`='Thomas', `last_name`='More' WHERE `id`='9140';
+UPDATE `contact` SET `first_name`='William', `last_name`='Blake' WHERE `id`='9141';
+UPDATE `contact` SET `title`='Mr', `first_name`='Charles', `last_name`='Dickens' WHERE `id`='9142';
+UPDATE `contact` SET `title`='Sir', `first_name`='Frank', `last_name`='Whittle' WHERE `id`='9143';
+UPDATE `contact` SET `title`='Mr', `first_name`='John', `last_name`='Peel' WHERE `id`='9144';
+UPDATE `contact` SET `title`='Mr', `first_name`='John', `last_name`='Logie Baird' WHERE `id`='9145';
+UPDATE `contact` SET `title`='Mr', `first_name`='Aneurin', `last_name`='Bevan' WHERE `id`='9146';
+UPDATE `contact` SET `title`='Sir', `first_name`='William', `last_name`='Wallace' WHERE `id`='9147';
+UPDATE `contact` SET `title`='Miss', `first_name`='Florence', `last_name`='Nightingale' WHERE `id`='9148';
+UPDATE `contact` SET `first_name`='Indira', `last_name`='Gandhi' WHERE `id`='9149';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Oprah', `last_name`='Winfrey' WHERE `id`='9150';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Hillary', `last_name`='Clinton' WHERE `id`='9151';
+UPDATE `contact` SET `title`='Miss', `first_name`='Brgitte', `last_name`='Bardot' WHERE `id`='9152';
+UPDATE `contact` SET `title`='Miss', `first_name`='Mata', `last_name`='Hari' WHERE `id`='9153';
+UPDATE `contact` SET `title`='Ms', `first_name`='Grace', `last_name`='Kelly' WHERE `id`='9154';
+UPDATE `contact` SET `title`='Ms', `first_name`='Ingrid', `last_name`='Bergman' WHERE `id`='9155';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Angelina', `last_name`='Jolie' WHERE `id`='9156';
+UPDATE `contact` SET `title`='Ms', `first_name`='Ellen', `last_name`='DeGeneres' WHERE `id`='9157';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Jane', `last_name`='Austen' WHERE `id`='9159';
+UPDATE `contact` SET `title`='Miss', `first_name`='Scarlett', `last_name`='Johansson' WHERE `id`='9158';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Harriet', `last_name`='Beecher Stowe' WHERE `id`='9160';
+UPDATE `contact` SET `title`='Ms', `first_name`='Mae', `last_name`='West' WHERE `id`='9161';
+UPDATE `contact` SET `first_name`='Agatha', `last_name`='Christie' WHERE `id`='9162';
+UPDATE `contact` SET `title`='Miss', `first_name`='Marie', `last_name`='Antoinette' WHERE `id`='9163';
+UPDATE `contact` SET `title`='Mdme', `first_name`='Marie', `last_name`='Curie' WHERE `id`='9164';
+UPDATE `contact` SET `first_name`='Emily', `last_name`='Murphy' WHERE `id`='9165';
+UPDATE `contact` SET `first_name`='Zsa Zsa', `last_name`='Gabor' WHERE `id`='9166';
+UPDATE `contact` SET `title`='Miss', `first_name`='Bette Davis' WHERE `id`='9167';
+UPDATE `contact` SET `title`='Miss', `first_name`='Helen', `last_name`='Keller' WHERE `id`='9168';
+UPDATE `contact` SET `first_name`='Emily', `last_name`='Bronte' WHERE `id`='9169';
+UPDATE `contact` SET `first_name`='Edith', `last_name`='Piaf' WHERE `id`='9171';
+UPDATE `contact` SET `title`='Miss', `first_name`='Amelia', `last_name`='Earhart' WHERE `id`='9172';
+UPDATE `contact` SET `first_name`='Raisa', `last_name`='Gorbachev' WHERE `id`='9173';
+UPDATE `contact` SET `title`='Miss', `first_name`='Bille', `last_name`='Holiday' WHERE `id`='9174';
+UPDATE `contact` SET `title`='Ms', `first_name`='Marlene', `last_name`='Dietrich' WHERE `id`='9175';
+UPDATE `contact` SET `first_name`='Rosa', `last_name`='Parks' WHERE `id`='9176';
+UPDATE `contact` SET `title`='Ms', `first_name`='Eva', `last_name`='Peron' WHERE `id`='9177';
+UPDATE `contact` SET `last_name`='Ringwald' WHERE `id`='9178';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Yoko', `last_name`='Ono' WHERE `id`='9179';
+UPDATE `contact` SET `title`='Ms', `first_name`='Germaine', `last_name`='Greer' WHERE `id`='9180';
+UPDATE `contact` SET `title`='Ms', `first_name`='Enid', `last_name`='Blyton' WHERE `id`='9181';
+UPDATE `contact` SET `title`='Mrs', `first_name`='Julie', `last_name`='Andrews' WHERE `id`='9184';
+UPDATE `contact` SET `title`='Mr', `first_name`='Enoch', `last_name`='Powell' WHERE `id`='9185';
+UPDATE `contact` SET `title`='Mr', `first_name`='Alexanger Graham', `last_name`='Bell' WHERE `id`='9186';
+
 
 
 
 -- Sync patient and contact names
 UPDATE patient p INNER JOIN contact c ON c.id = p.contact_id SET p.gender =
 CASE
-    WHEN c.title IN ('Mr', 'mr', 'master') THEN 'M'
-    WHEN c.title IN ('Mrs', 'Miss', 'Ms') THEN 'F'
+    WHEN c.title IN ('Mr', 'mr', 'master', 'sir', 'Sir', 'Lord', 'Captain', 'Cptn') THEN 'M'
+    WHEN c.title IN ('Mrs', 'Miss', 'Ms', 'Mdme', 'Dame', 'Madame', 'Lady', 'HRH') THEN 'F'
     ELSE 'U'
 END;
